4e608bd8eb09882b45b4b3653703749b49fc2c7f
Ahmet Karadağ <akkaradaga@gmail.com>
2025-06-01 21:59:04 +0300

    Initial Commit

---
+++ NoteApp/NoteAppApp.swift
@@ -0,0 +1,20 @@
+//
+//  NoteAppApp.swift
+//  NoteApp
+//
+//  Created by ahmet karadağ on 1.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct NoteAppApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
